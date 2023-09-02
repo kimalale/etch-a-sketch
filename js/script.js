3ed9f8a53d531241322ad5d97209f66e08725c27
Kim Malale <kimalale@hotmail.com>
2023-09-02 17:32:43 +0200

    Refactoring codebase

--- js/script.js
+++ js/script.js
@@ -13,6 +13,9 @@ sliderValue.classList.add("sliderValue"); // Center text value
 sliderPreview.appendChild(sliderValue); // Add to the DOM
 
 
+
+
+
 const random_hex_color_code = () => {
     let n = (Math.random() * 0xfffff * 1000000).toString(16);
     return '#' + n.slice(0, 6);
@@ -28,6 +31,8 @@ let singleRandom = true;
 const colorPick = document.querySelector(".color-selector");
 const randomColor = document.querySelector("#colorID");
 
+
+
 //Update the value on html, if changed
 slider.oninput = () => {
 
@@ -37,28 +42,22 @@ slider.oninput = () => {
     remSquare.forEach( tile => {
         tile.remove();
     })
-    
+
     let widthHeight = Math.floor(706/`${slider.value}`);
 
 
     for (let i= 0; i < slider.value * slider.value ; i++)
     {
-    squares[i] = document.createElement('div'); 
+    squares[i] = document.createElement('div');
 
     board.style = `grid-template-columns: repeat(${slider.value}, 1fr); grid-template-rows: repeat(${slider.value},1fr);`;
 
     squares[i].classList.add("grid-item");
     board.appendChild(squares[i]);
     }
-
-    squares.map( (square) => {
-        square.addEventListener('mouseover', () => {
-            square.style = `background-color: ${(singleRandom) ? colorPick.value : random_hex_color_code()}`;
-        })
-    })
-
 }
 
+let isDrawing = false;
 
 for (let i= 0; i < slider.value*slider.value ; i++)
 {
@@ -67,59 +66,93 @@ for (let i= 0; i < slider.value*slider.value ; i++)
     board.appendChild(squares[i]);
 }
 
-squares.map( (square) => {
-    square.addEventListener('mouseover', () => {
-        let color;
-        square.style = `background-color: ${(singleRandom) ? colorPick.value : random_hex_color_code()}`;
-    })
+let isPencilTool = true;
+
+board.addEventListener('mousedown', () => {
+        isDrawing = true;
 })
+
+board.addEventListener('mouseup', () => {
+        isDrawing = false;
+    })
+
+
+board.addEventListener('mousemove', (e) => {
+        if (!isDrawing) return;
+
+        if (e.target != board)
+        {
+        if (isPencilTool)
+            e.target.style = `background-color: ${(singleRandom) ? colorPick.value : random_hex_color_code()}`;
+        else
+            e.target.style = "#fcfcfc";
+        }
+})
+
+
+
 
 colorPick.oninput = () => {
     if(!singleRandom) {
         singleRandom = true;
     }
 
+    colorPick.classList.toggle('button-33_toggle_state');
+    if (randomColor.classList.contains('button-33_toggle_state'))
+    {
+        randomColor.classList.remove('button-33_toggle_state');
+    }
 }
-
 
 randomColor.addEventListener('click', ()=>{
     singleRandom = false;
+    randomColor.classList.toggle('button-33_toggle_state');
+    if (colorPick.classList.contains('button-33_toggle_state'))
+    {
+        colorPick.classList.remove('button-33_toggle_state');
+    }
 })
 
-//color picker
 
 const pencil = document.querySelector("#pencil");
-// pencil.addEventListener('click', () => )
-
-const eraser = document.querySelector("#eraser");
-
-eraser.addEventListener('click', () => {
-
-    squares.map((square) => {
-        square.addEventListener('mouseover', () => {
-
-            square.style = "#fcfcfc";
-        })
-    })});
 
 pencil.addEventListener('click', () => {
+    pencil.classList.toggle('button-33_toggle_state');
+    isPencilTool = true;
+    if (eraser.classList.contains('button-33_toggle_state') || bucket.classList.contains('button-33_toggle_state'))
+       {
+        eraser.classList.remove('button-33_toggle_state');
+        bucket.classList.remove('button-33_toggle_state');
+       }
+})
 
-    squares.map((square) => {
-        square.addEventListener('mouseover', () => {
+const eraser = document.querySelector("#eraser")
 
-            square.style = `background-color: ${(singleRandom) ? colorPick.value : random_hex_color_code()}`;
-        })
-    })
+eraser.addEventListener('click', () => {
+    eraser.classList.toggle('button-33_toggle_state');
+    isPencilTool = false;
+    if (pencil.classList.contains('button-33_toggle_state') || bucket.classList.contains('button-33_toggle_state'))
+    {
+        pencil.classList.remove('button-33_toggle_state');
+        bucket.classList.remove('button-33_toggle_state');
+    }
 });
+
+
 
 const bucket = document.querySelector("#bucket");
 
 bucket.addEventListener('click', () => {
-
+    bucket.classList.toggle('button-33_toggle_state');
     squares.map((square) => {
         square.style = `background-color: ${(singleRandom) ? colorPick.value : random_hex_color_code()}`;
-    
+
     })
+    if (eraser.classList.contains('button-33_toggle_state') || pencil.classList.contains('button-33_toggle_state'))
+    {
+     eraser.classList.remove('button-33_toggle_state');
+     pencil.classList.remove('button-33_toggle_state');
+    }
 });
 
 const reset = document.querySelector("#reset");
